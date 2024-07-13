8b29f9494dcccf9af1816301c57c45f2f6f97b84
weiyang <weiyang_11@outlook.com>
2024-07-13 23:25:02 +0800

    number type

---
+++ Type/typescript-type/src/types/numberType.ts
@@ -0,0 +1,17 @@
+let number :number = 100 ;
+
+number = 28.7;
+
+number = -300;
+
+number = 0b1010;
+
+number = Infinity * .01;
+
+number = NaN;
+
+number = parseInt('12');
+
+number = parseFloat('12.5');
+
+number = Number('12');
