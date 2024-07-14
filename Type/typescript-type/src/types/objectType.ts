64265be9b8f077e91c190a9ad407851222dbc256
weiyang <weiyang_11@outlook.com>
2024-07-14 13:41:58 +0800

    Object Type

---
+++ Type/typescript-type/src/types/objectType.ts
@@ -0,0 +1,26 @@
+let person = {
+  name: 'John',
+  age: 25,
+};
+
+let car:{
+  brand: string;
+  color: string;
+} = {
+  brand: 'Ford',
+  color: 'Red',
+};
+
+
+let acticle : {
+  author : string;
+  content : string;
+  title: string;
+  image?: string //optional property
+}
+
+acticle = {
+  author: 'Joe',
+  content: 'Typescript tutorial',
+  title: 'Introduction to Typescript'
+}
