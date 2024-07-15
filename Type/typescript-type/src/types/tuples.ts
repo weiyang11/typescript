e2c4b441bb7de9bd6a386969870066efdc147843
weiyang <weiyang_11@outlook.com>
2024-07-16 01:32:48 +0800

    tuple type

---
+++ Type/typescript-type/src/types/tuples.ts
@@ -0,0 +1,9 @@
+let person1: [string,string,number?]; // Tuple
+
+person1 = ["John", "Doe", 30];
+
+person1 = ["John", "Doe"];
+
+type listOfStudents = [number, boolean, ...string[]];
+
+let student1: listOfStudents = [2, true, "John", "Doe"];
