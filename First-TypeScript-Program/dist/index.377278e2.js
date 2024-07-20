8770fbab47a927f94242eb9bce2721ad791a02df
weiyang <weiyang_11@outlook.com>
2024-07-21 00:37:35 +0800

    syallabus renew

--- First-TypeScript-Program/dist/index.377278e2.js
+++ First-TypeScript-Program/dist/index.377278e2.js
@@ -1,3 +1,20 @@
 console.log("hello");
+const students = [
+    {
+        name: "John",
+        age: 25
+    },
+    {
+        name: "Jane",
+        age: 24
+    }
+];
+for (const student of students)console.log(student.name);
+//@ts-ignore, stop throwing errors for the next line
+function addNumbers(a, b) {
+    return a + b;
+} //a and b are of type any
+console.log(addNumbers(1, 2)); // 3
+console.log(addNumbers("text", 2)); // text2
 
 //# sourceMappingURL=index.377278e2.js.map
--- First-TypeScript-Program/src/index.ts
+++ First-TypeScript-Program/src/index.ts
@@ -1,1 +1,26 @@
 console.log('hello');
+
+
+const students = [
+  {
+  name: "John",
+  age: 25,
+},
+{
+  name: "Jane",
+  age: 24
+}
+]
+
+for (const student of students) {
+  console.log(student.name);
+  // console.log(student.grade); // Error, property 'grade' does not exist on type '{ name: string; age: number; }'
+}
+
+//@ts-ignore, stop throwing errors for the next line
+function addNumbers(a, b) {
+  return a + b;
+} //a and b are of type any
+
+console.log(addNumbers(1, 2)); // 3
+console.log(addNumbers("text", 2)); // text2
