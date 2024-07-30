4e907845c4e5c95fa91586cc243c4deb8cb49d2d
weiyang <weiyang_11@outlook.com>
2024-07-30 23:24:36 +0800

    refactoring the constructor method by adding extra private function

--- classes-and-inheritance/src/index.ts
+++ classes-and-inheritance/src/index.ts
@@ -1,37 +1,38 @@
 class Person2 {
-  private _age: number | undefined;
 
-  constructor(private name: string) {
-    // if(age < 0 || age> 200){
-    //   throw new Error('The age is invalid');
-    // }
-  }
-
-  public getName(){
-    return this.name;
-  }
-
-  public set age(age: number){
+  private testUsersAge(age: number){
     if(age < 0 || age> 200){
       throw new Error('The age is invalid');
     }
+    return age;
+  }
+
+  constructor(private _name: string, private _age : number) {
+    this.testUsersAge(_age);
+    this._age = _age;
+  }
+  public set name(name: string){
+    this._name = name;
+  }
+
+  public get name(){
+    return this._name;
+  }
+
+  public set age(age: number){
+    this.testUsersAge(age);
     this._age = age;
   }
 
   public get age(){
-    if(this._age == undefined){
-      throw new Error('The age is not set yet');
-    }
     return this._age;
   }
 }
 
-const person4: Person2 = new Person2('John');
+const person4: Person2 = new Person2('John', 32);
+
+// person.age = 201; // Error: The age is invalid
 
 person4.age = 30; // set the age
 
 console.log(person4); // Person2 { name: 'John', _age: 30 }
-
-const mark: Person2 = new Person2('Mark');
-
-console.log(mark.age); // Error: The age is not set yet
